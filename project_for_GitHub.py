1730174a529aabf6a7f142c1732ab0632005cb3b
rik-code <topemven1234567890@gmail.com>
2022-05-05 17:29:59 +0300

    initial commit

--- project_for_GitHub.py
+++ project_for_GitHub.py
@@ -1,6 +1,7 @@
 import telebot
+from your_key import key
 
-bot = telebot.TeleBot('your-key')
+bot = telebot.TeleBot(key)
 
 @bot.message_handler(content_types=['text'])
 
@@ -11,6 +12,12 @@ def get_text_messages(message):
     elif message.text == '/help':
         answer = 'Напиши привет'
         bot.send_message(message.from_user.id, answer)
+    elif message.text == '/check-GitHub':
+        response = r.get('https://github.com')
+        if response.status_code == 200:
+            bot.send_message(message.from_user.id, 'Доступен')
+        else:
+            bot.send_message(message.from_user.id, 'недоступен')
     else:
         answer = 'я вас не понимаю'
         bot.send_message(message.from_user.id, answer)
